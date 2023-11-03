79cbaeab82e8f0acb63bb60e1e23576cdac51699
MidKnightXI <romain.bouguet@epitech.eu>
2023-11-03 17:43:10 +0100

    chores: lint

--- Program.cs
+++ Program.cs
@@ -12,8 +12,14 @@ internal class Program
     private static async Task<int> Main(string[] args)
     {
         var rootCommand = new RootCommand("WhichCam - Camera Model/Maker detector");
-        var target = new Option<DirectoryInfo>("--target", "The directory containing the images to analyze."){ IsRequired = true };
-        var output = new Option<FileInfo>("--output", "The output file path."){ IsRequired = true };
+        var target = new Option<DirectoryInfo>(
+            name: "--target",
+            description: "The directory containing the images to analyze."){
+            IsRequired = true };
+        var output = new Option<FileInfo>(
+            name: "--output",
+            description: "The output file path."){
+            IsRequired = true };
 
         rootCommand.AddOption(target);
         rootCommand.AddOption(output);
