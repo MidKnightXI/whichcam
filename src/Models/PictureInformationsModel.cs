eb4f193a7585a3c9186013201e0520f701e5b667
MidKnightXI <romain.bouguet@epitech.eu>
2023-11-09 14:04:24 +0100

    feat: adding output model

---
+++ src/Models/PictureInformationsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WhichCam;
+
+public class CameraInformations
+{
+    public string? Maker { get; set; }
+    public string? Model { get; set; }
+}
+
+public class PictureInformationsModel
+{
+    public required bool Success { get; set; }
+    public required string PictureFullName { get; set; }
+    public required CameraInformations Detected { get; set; }
+}
